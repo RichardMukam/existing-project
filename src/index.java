559b16b8b5a417369c04d6a2e630c79d037e30b6
RichardMukam <77300333+RichardMukam@users.noreply.github.com>
2022-08-13 23:52:10 -0400

    Update index.java

--- src/index.java
+++ src/index.java
@@ -1,8 +1,17 @@
 package src;
+import java.util.Scanner;
 public class index {
   public static void main(String[] args)
   {
+    Scanner scanner = new Scanner(System.in);
     System.out.println("hi");
     System.out.println("bye");
+    System.out.println("");
+    System.out.println("What is your name? ");
+    String first_name = scanner.nextLine();
+    System.out.println("What is your last name's initial? ");
+    String last_name = scanner.nextLine();
+    System.out.println("Hi " + first_name + " " + last_name + ", welcome to GitHub!");
+    scanner.close();
   }
 }
